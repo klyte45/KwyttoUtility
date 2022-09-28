c6661db451b55e724168b68bce4d3484f6229609
Leandro Klyte <klyte45@hotmail.com>
2022-09-28 09:45:09 -0300

    Fixing bridge connection resolver

--- Utils/BridgeUtils.cs
+++ Utils/BridgeUtils.cs
@@ -8,7 +8,31 @@ namespace Kwytto.Utils
     {
         public static T GetMostPrioritaryImplementation<T>() where T : class, IBridgePrioritizable
         {
-            return AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => typeof(T).IsAssignableFrom(p)).Select(x =>
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var classes = assemblies.SelectMany(s =>
+            {
+                try
+                {
+                    //LogUtils.DoLog($"Loading Assembly: {s.FullName}");
+                    return s.GetExportedTypes();
+                }
+                catch
+                {
+                    return new Type[0];
+                }
+            }).Where(p =>
+            {
+                try
+                {
+                    return typeof(T).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract;
+                }
+                catch { return false; }
+            }).ToArray();
+            if (BasicIUserMod.DebugMode)
+            {
+                LogUtils.DoLog($"Found Classes of {typeof(T)} ({classes?.Count()}):\n\t{string.Join("\n\t", classes.Select(x => x.FullName).ToArray())}");
+            }
+            return classes.Select(x =>
             {
                 try
                 {
@@ -18,7 +42,7 @@ namespace Kwytto.Utils
                 {
                     return null;
                 }
-            }).Where(x => x?.IsBridgeEnabled ?? false).OrderBy(x => x.Priority).FirstOrDefault();
+            }).Where(x => x?.IsBridgeEnabled ?? false).OrderBy(x => x.Priority).First();
         }
     }
 }
