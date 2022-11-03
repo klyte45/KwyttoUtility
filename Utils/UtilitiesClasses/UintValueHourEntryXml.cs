37d7319711657a74f0083af9335b20aa3fc9977e
Leandro Klyte <klyte45@hotmail.com>
2022-11-03 20:51:10 -0300

    Added hour value structure

---
+++ Utils/UtilitiesClasses/UintValueHourEntryXml.cs
@@ -0,0 +1,38 @@
+﻿using Kwytto.Interfaces;
+using System;
+
+using System.Xml.Serialization;
+
+
+namespace Kwytto.Utils
+{
+    public class UintValueHourEntryXml<T> : ITimeable<T> where T : UintValueHourEntryXml<T>
+    {
+        private int m_hourOfDay;
+        private uint m_value;
+
+        [XmlAttribute("startTime")]
+        public int? HourOfDay
+        {
+            get => m_hourOfDay;
+            set
+            {
+                m_hourOfDay = (value ?? -1) % 24;
+                OnEntryChanged?.Invoke((T)this);
+            }
+        }
+
+        [XmlAttribute("value")]
+        public uint Value
+        {
+            get => m_value;
+            set
+            {
+                m_value = value;
+                OnEntryChanged?.Invoke((T)this);
+            }
+        }
+
+        public event Action<T> OnEntryChanged;
+    }
+}
